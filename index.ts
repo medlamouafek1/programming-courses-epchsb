5f3467f249663ff0c9ef7b02eb47ad2d5abf3c29
medlamouafek1 <118979075+medlamouafek1@users.noreply.github.com>
2022-11-24 14:00:14 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let num: number;
+let num2: number;
+let num3: number;
+let num7: number;
+num = 100;
+num2 = 400;
+num3 = 1476374838;
+num7 = 1223344;
+console.log(num + num2);
+console.log(num - num2);
+console.log(num / num2);
+console.log(num3 * num - num2);
+console.log((num2 * num7) / num);
